ed32792a894d57032db342e5340ac6bfb284d9c7
MatsukySo <mateusassissouzaa@gmail.com>
2025-01-22 19:27:36 -0400

    FaseTeste

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -5,6 +5,6 @@ import model.entities.Departmant;
 public class Main {
     public static void main(String[] args) {
         Departmant obj = new Departmant(1,"Books");
-        System.out.println(obj);
+        System.out.println("Obj: " + obj);
     }
 }
