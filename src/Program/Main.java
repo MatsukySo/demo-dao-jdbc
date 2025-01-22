82f99b57b77deabfe4f8949cb648e0889c110369
MatsukySo <mateusassissouzaa@gmail.com>
2025-01-22 19:41:36 -0400

    Fase2

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,10 +1,15 @@
 package Program;
 
 import model.entities.Departmant;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Departmant obj = new Departmant(1,"Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
     }
 }
