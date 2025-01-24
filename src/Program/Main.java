ac808eaa52da4ef312425403ace120e657fcb69a
MatsukySo <mateusassissouzaa@gmail.com>
2025-01-23 22:24:05 -0400

    FaseMeio1

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -5,14 +5,21 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.creatSellerDao();
 
+        System.out.println("==== TEST 1: seller finfById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println("\n==== TEST 2: seller finfByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
